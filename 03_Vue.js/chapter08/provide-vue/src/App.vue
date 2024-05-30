01360e81b190607f06d024930950fb9fc7010a92
문준일 <answnsdlfwkd@naver.com>
2024-05-30 14:08:55 +0900

    240530 오전 커밋

---
+++ 03_Vue.js/chapter08/provide-vue/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Child from './components/Child.vue';
+</script>
+
+<template>
+  <div>
+    <Child :drilling="drilling" />
+  </div>
+</template>
+
+<script>
+export default {
+  name:'App',
+  data() {
+    return{
+      drilling: '어느정도높이까지내려가는거예요???',
+    };
+  },
+  provide() {
+    return {
+      fly: 'App에서 맨 밑에 Child까지 한방에 올 수있는 방법은??? Provide!!!'
+    }
+  }
+};
+</script>
---
+++ 03_Vue.js/chapter08/provide-vue/src/components/GrandChild.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>child -> child -> GrandChild 까지 드릴링해서 내려온 props 의 내용은?</h1>
+        <h2>{{ drilling }}</h2>
+        <h2>{{ fly }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GrandChild',
+        props: ['drilling'],
+        inject: ['fly'],
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+  
