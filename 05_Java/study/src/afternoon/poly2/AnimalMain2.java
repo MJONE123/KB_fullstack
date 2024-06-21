e65144c613f2c686bc957baf0aa1e281574036d1
문준일 <answnsdlfwkd@naver.com>
2024-06-21 17:40:35 +0900

    0621 수업 커밋

---
+++ 05_Java/study/src/afternoon/poly2/AnimalMain2.java
@@ -0,0 +1,18 @@
+package afternoon.poly2;
+
+public class AnimalMain2 {
+    public static void main(String[] args) {
+        Animal[] animals = {new Cat(), new Dog(), new Donkey(), new Chicken()};
+        for (Animal animal : animals) {
+            soundAnimal(animal);
+        }
+
+    }
+
+    private static void soundAnimal(Animal animal) {
+        animal.sound();
+    }
+}
+
+
+
