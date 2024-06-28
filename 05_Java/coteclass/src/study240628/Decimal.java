0c7d36a303d1735c4e516e517ee3f29aa5c46647
문준일 <answnsdlfwkd@naver.com>
2024-06-28 15:38:47 +0900

    헷갈리는 부분 주석 추가

--- 05_Java/coteclass/src/study240628/Decimal.java
+++ 05_Java/coteclass/src/study240628/Decimal.java
@@ -6,6 +6,7 @@ class Decimal {
         int counter = 0;
 
         for (int i = 0; i < nums.length - 2; i++) {
+            //i,j,k 완전 탐색 (하나씩 다 더해보는 것)
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     if (isPrime(nums[i] + nums[j] + nums[k])) {
