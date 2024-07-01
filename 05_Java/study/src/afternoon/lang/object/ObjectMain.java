f43bd5e8ce4fccac3104ef4433924bf1e9378dc4
문준일 <answnsdlfwkd@naver.com>
2024-07-01 14:12:52 +0900

    java.lang 예재

---
+++ 05_Java/study/src/afternoon/lang/object/ObjectMain.java
@@ -0,0 +1,16 @@
+package afternoon.lang.object;
+
+public class ObjectMain {
+    public static void main(String[] args) {
+        Child child = new Child();
+
+        child.childMethod();
+        child.parentMethod();
+
+        System.out.println(child.toString());
+        System.out.println(child);
+
+    }
+}
+
+
