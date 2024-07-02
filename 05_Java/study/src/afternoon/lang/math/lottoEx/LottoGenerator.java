d0439356c8bd2683098e2f3d19d54fd1c38766d3
문준일 <answnsdlfwkd@naver.com>
2024-07-02 10:18:59 +0900

    로또 실습 완료

--- 05_Java/study/src/afternoon/lang/math/lottoEx/LottoGenerator.java
+++ 05_Java/study/src/afternoon/lang/math/lottoEx/LottoGenerator.java
@@ -1,4 +1,48 @@
 package afternoon.lang.math.lottoEx;
 
+import java.util.Random;
+
 public class LottoGenerator {
+    private static final int LOTTO_SIZE = 6;
+    private static final int MAX_NUMBER = 45;
+
+    private int[] lottoNumbers;
+
+    public LottoGenerator() {
+        lottoNumbers = new int[LOTTO_SIZE];
+    }
+
+    public void generate() {
+        Random random = new Random();
+        int count = 0;
+
+        while (count < LOTTO_SIZE) {
+            int randomNumber = random.nextInt(MAX_NUMBER) + 1;
+
+            if (!contains(randomNumber)) {
+                lottoNumbers[count] = randomNumber;
+                count++;
+            }
+        }
+    }
+
+    private boolean contains(int number) {
+        for (int i = 0; i < LOTTO_SIZE; i++) {
+            if (lottoNumbers[i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printLottoNumbers() {
+        System.out.print("로또 번호: ");
+        for (int i = 0; i < LOTTO_SIZE; i++) {
+            System.out.print(lottoNumbers[i]);
+            if (i < LOTTO_SIZE - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
 }
