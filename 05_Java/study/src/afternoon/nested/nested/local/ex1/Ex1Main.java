65faf47eb11fd5fce89bab2696f36eb6d47dcb92
문준일 <answnsdlfwkd@naver.com>
2024-07-03 17:16:18 +0900

    Ex1과 Ex2 일단 먼저 커밋

---
+++ 05_Java/study/src/afternoon/nested/nested/local/ex1/Ex1Main.java
@@ -0,0 +1,33 @@
+package afternoon.nested.nested.local.ex1;
+
+import java.util.*;
+
+public class Ex1Main {
+    public static void helloDice() {
+        System.out.println("프로그램 시작");
+
+
+        // 코드 조각 시작
+        int rand = new Random().nextInt(6) + 1;
+        System.out.println("주사위의 값은 :" + rand);
+        //
+        System.out.println("프로그램 종료");
+    }
+
+    public static void helloDiceSum() {
+        System.out.println("프로그램 시작");
+
+        //코드 조각 시작
+        int rand1 = new Random().nextInt(6) + 1;
+        int rand2 = new Random().nextInt(6) + 1;
+        int sum = rand1 + rand2;
+
+        System.out.println("주사위를 투 번 굴린 값의 합은: " + sum);
+    }
+
+
+    public static void main(String[] args) {
+        helloDice();
+        helloDiceSum();
+    }
+}
---
+++ 05_Java/study/src/afternoon/nested/nested/local/ex1/Ex2Main.java
@@ -0,0 +1,41 @@
+package afternoon.nested.nested.local.ex1;
+
+import java.util.*;
+
+public class Ex2Main {
+    public static void complicatedProgram(Dice dice) {
+        dice.run();
+
+        System.out.println("복잡한 과정 시작");
+        System.out.println("복잡한 과정 종료 후, 원하는 기능 실행");
+
+        // 여기서 원하는 프로그램 실행
+        dice.run();
+
+        System.out.println("복잡한 과정 다시 시작");
+        System.out.println("복잡한 과정 종료 후, 프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        complicatedProgram(new DiceOnce());
+        complicatedProgram(new DiceSum());
+    }
+
+    static class DiceOnce implements Dice {
+        @Override
+        public void run() {
+            int rand = new Random().nextInt(6) + 1;
+            System.out.println("주사위의 값은 : " + rand);
+        }
+    }
+
+    static class DiceSum implements Dice {
+        @Override
+        public void run() {
+            int rand1 = new Random().nextInt(6) + 1;
+            int rand2 = new Random().nextInt(6) + 1;
+            int sum = rand1 + rand2;
+            System.out.println("두 주사위의 합은 : " + sum);
+        }
+    }
+}
