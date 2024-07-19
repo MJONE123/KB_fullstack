46cdba23510efa7ae9c9bd9bd97ba370ac3ce54f
문준일 <answnsdlfwkd@naver.com>
2024-07-19 17:52:30 +0900

    0719 코테특강  커밋

---
+++ 05_Java/coteclass/src/study240719/N_Queen.java
@@ -0,0 +1,14 @@
+package study240719;
+
+public class N_Queen {
+    public int solution(int n) {
+        int answer = 0;
+
+
+
+
+
+
+        return answer;
+    }
+}
---
+++ 05_Java/coteclass/src/study240719/TargetNumber.java
@@ -0,0 +1,17 @@
+package study240719;
+
+public class TargetNumber {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+
+
+
+
+
+
+
+
+
+        return answer;
+}
