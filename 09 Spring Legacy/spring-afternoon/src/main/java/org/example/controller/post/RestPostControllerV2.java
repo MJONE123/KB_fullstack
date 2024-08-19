f43f3a82a0f53022f80a6a3af536e2aaea3d3e15
문준일 <answnsdlfwkd@naver.com>
2024-08-19 17:23:17 +0900

    responseEntity 테스트

--- 09 Spring Legacy/spring-afternoon/src/main/java/org/example/controller/post/RestPostControllerV2.java
+++ 09 Spring Legacy/spring-afternoon/src/main/java/org/example/controller/post/RestPostControllerV2.java
@@ -4,33 +4,42 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dto.post.PostDto;
 import org.example.dto.post.PostRepository;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
-@RequestMapping("/post/v1/rest")
-public class RestPostController {
+@CrossOrigin(origins = "*")
+@RequestMapping("/post/v2/rest")
+public class RestPostControllerV2 {
     private final PostRepository postRepository;
+    private final HttpSession httpSession;
     private String context = "/post";
 
     // 게시글 목록 보기
     @GetMapping("/show")
-    public List<PostDto> postList(HttpServletRequest request) {
+    public ResponseEntity<List<PostDto>> postList(HttpServletRequest request) {
         log.info("=========> 게시글 목록 페이지 호출, " + request.getRequestURI());
 
         List<PostDto> list = postRepository.findAll();
-
-        return list;
+        //ok =  200번
+        return ResponseEntity.ok(list);
     }
 
-    
+    @GetMapping(value = "/test", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("요청을 처리할 수 없습니다");
+
+    }
+
+
     // 게시물 조건 조회
     @GetMapping("/search")
     public List<PostDto> postSearch(
@@ -99,16 +108,20 @@ public class RestPostController {
     }
 
     // 삭제 기능 컨트롤러
-    @PostMapping("/delete")
-    public String postDelete(@RequestParam("id") String id, HttpServletRequest request) {
-        log.info("================> 게시글 삭제 기능 호출, " + request.getRequestURI());
 
-        long postId = Long.parseLong(id);
-        int affectedRows = postRepository.delete(postId);
+    @DeleteMapping(value = "/delete/{id}", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<String> postDelete(@PathVariable("id") Long id, HttpServletRequest request) {
+        log.info("--------> 게시글 삭제 기능 호출, " + request.getRequestURI());
 
-        if (affectedRows > 0) log.info("삭제 성공");
+        // 삭제 요청 및 삭제 요청에 대한 결과를 affectedRows로 받기
+        int affectedRows = postRepository.delete(id);
 
-        return "redirect:/post/v1/show";
+        if(affectedRows > 0) {
+            return ResponseEntity.ok("삭성(삭제 성공이라는 뜻)");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("요청을 처리할 수 없습니다!");
+        }
     }
 }
+
 
