7753148c8d1277d51b1d975d81af2133c4394fad
문준일 <answnsdlfwkd@naver.com>
2024-08-20 17:33:22 +0900

    book 저장

--- 09 Spring Legacy/spring-afternoon/src/main/java/org/example/repository/book/jpa/JpaBookRepository.java
+++ 09 Spring Legacy/spring-afternoon/src/main/java/org/example/repository/book/jpa/JpaBookRepository.java
@@ -2,8 +2,31 @@ package org.example.repository.book.jpa;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.domain.Book;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
+@Repository
+@Slf4j
 public class JpaBookRepository {
+    private final EntityManager em;
+
+    public List<Book> findAll() {
+        String jpql = "select b from Book b";
+        List<Book> bookList = em.createQuery(jpql, Book.class).getResultList();
+        return bookList;
+    }
+
+    public Book save(Book book) {
+        em.persist(book);
+        return book;
+    }
+
+    
 }
