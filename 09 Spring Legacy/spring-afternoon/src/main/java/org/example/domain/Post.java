b615eff2629d398cb17462a4c3edcdedac31cb25
문준일 <answnsdlfwkd@naver.com>
2024-08-23 14:49:37 +0900

    main/java/org/example/domain

---
+++ 09 Spring Legacy/spring-afternoon/src/main/java/org/example/domain/Post.java
@@ -0,0 +1,28 @@
+package org.example.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // or another appropriate strategy
+    private Long id;
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Column(name = "title")
+    private String title;
+    @Column(name = "content")
+    private String content;
+}
