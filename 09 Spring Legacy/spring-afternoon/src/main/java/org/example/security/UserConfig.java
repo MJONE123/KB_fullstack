8023dc2e011168578c10dfe0961f34c1c80db4ea
문준일 <answnsdlfwkd@naver.com>
2024-08-26 17:42:14 +0900

    암호화(근데 안됨)

--- 09 Spring Legacy/spring-afternoon/src/main/java/org/example/security/UserConfig.java
+++ 09 Spring Legacy/spring-afternoon/src/main/java/org/example/security/UserConfig.java
@@ -1,12 +1,22 @@
 package org.example.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class UserConfig implements WebMvcConfigurer {
+
+    //암호화 라이브러리 Bean 등록!
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         System.out.println("시큐리티 인터셉터가 등록되었습니담");
--- 09 Spring Legacy/spring-afternoon/src/main/java/org/example/service/user/UserService.java
+++ 09 Spring Legacy/spring-afternoon/src/main/java/org/example/service/user/UserService.java
@@ -4,12 +4,14 @@ package org.example.service.user;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.User;
 import org.example.repository.user.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -17,11 +19,15 @@ public class UserService {
     }
 
     public boolean isPasswordValid(User user, String rawPassword) {
-        return rawPassword.equals(user.getPassword());
+        return passwordEncoder.matches(rawPassword, user.getPassword());
     }
 
+
     public User save(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         user.setRoles("ROLE_MEMBER");
         return userRepository.save(user);
     }
+
 }
